80145d862aa66a30adcbb85b9f3e736854a5ca44
fortuinp <pinda.fortuin@gmail.com>
2024-01-10 13:11:37 +0200

    contact

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,9 +1,94 @@
 <template>
-      <div class="contacts vh-100">
-       <h2 class="display-2">
-        Contacts
-       </h2>
-     </div>
+      <main class="container-fluid vh-100" id="contact">
+
+<section class=" container mb-4">
+
+  <h2 class="h1-responsive font-weight-bold  my-4 text-start mx-5">Contact us</h2>
+
+  <div class="row">
+
+
+    <div class="col-md-9 mb-md-0 mb-5">
+     
+
+        <div class="row">
+          <div class="col-md-6">
+            <div class="md-form mb-0">
+              <input type="text" id="name" name="name" class="form-control">
+              <label for="name" class="">Your name</label>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="md-form mb-0">
+              <input type="text" id="email" name="email" class="form-control">
+              <label for="email" class="">Your email</label>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="md-form mb-0">
+              <input type="text" id="subject" name="subject" class="form-control">
+              <label for="subject" class="">Subject</label>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+
+          <div class="col-md-12">
+
+            <div class="md-form">
+              <textarea type="text" id="message" name="message" rows="2"
+                class="form-control md-textarea"></textarea>
+              <label for="message">Your message</label>
+            </div>
+
+          </div>
+        </div>
+
+
+ 
+
+      <div class="text-center text-md-left">
+        <a id="btncontact" class="btn btn-secondary"
+          onclick="document.getElementById('contact-form').submit();">Send</a>
+      </div>
+      <div class="status"></div>
+    </div>
+
+    <div class="col-md-3 text-center">
+      <ul class="list-unstyled mb-0">
+        <li><i class="fas fa-map-marker-alt fa-2x"></i>
+          <p>7 Dorr Street, Cape Town, SA</p>
+        </li>
+
+        <li><i class="fas fa-phone mt-4 fa-2x"></i>
+          <p>021 361 3404</p>
+        </li>
+
+        <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+          <p>fortuinpinda@gmail.com</p>
+        </li>
+        <li>
+          <a target="_blank" href="https://www.linkedin.com/in/pinda-fortuin"><i id="icon"
+              class="bi bi-linkedin"></i></a>
+          <a target="_blank" href="https://github.com/fortuinp"><i id="icon" class="bi bi-github"></i></a>
+
+
+        </li>
+      </ul>
+    </div>
+
+
+  </div>
+
+</section>
+</main>
   
 </template>
 
